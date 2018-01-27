022f94b9f607d47d7207ee499aa91070177f7d65
BobcatBill <bill@rowanfamily.com>
2018-01-27 00:01:01 -0600

    Initial File

---
+++ init_script.sh
@@ -0,0 +1,48 @@
+DATE=$(date)
+ROLE=$(whoami)
+
+if [[ $ROLE != "root" ]]; then
+	echo "$DATE [ERROR] - You must be root to run this script"
+	exit 1
+fi
+
+if [[ $1 = "" ]]; then
+	echo "$DATE [ERROR] - No customer name supplied"
+	exit 1
+fi
+
+echo "$DATE [INFO] - Creating customer directories for $1"
+mkdir /opt/$1
+mkdir /opt/$1/data
+mkdir /opt/$1/scripts
+
+echo "$DATE [INFO] - Changing permissions to skyboxview user"
+chown -R skyboxview:skyboxview /opt/$1
+
+echo "$DATE [INFO] - Changing swappiness"
+sysctl vm.swappiness=0
+
+echo "$DATE [INFO] - Adding custom aliases"
+ALIASCHECK=$(grep 'alias sdebug' /home/skyboxview/.bashrc)
+if [[ $ALIASCHECK != "" ]]; then
+	echo "$DATE [INFO] - sdebug alias already exists.  Skipping..."
+else
+	echo "alias sdebug=‘tail -f /opt/skyboxview/server/log/debug/debug.log’" >> /home/skyboxview/.bashrc
+fi
+
+ALIASCHECK=$(grep 'alias cdebug' /home/skyboxview/.bashrc)
+if [[ $ALIASCHECK != "" ]]; then
+        echo "$DATE [INFO] - cdebug alias already exists.  Skipping..."
+else
+	echo "alias cdebug=‘tail -f /opt/skyboxview/collector/log/debug/debug.log’" >> /home/skyboxview/.bashrc
+fi
+
+ALIASCHECK=$(grep 'alias specget' /home/skyboxview/.bashrc)
+if [[ $ALIASCHECK != "" ]]; then
+        echo "$DATE [INFO] - specget alias already exists.  Skipping..."
+else
+	echo "alias specget=“grep ‘routing table speculation’ /opt/skyboxview/server/log/server.log | awk -F’speculation on’ ‘{print " >> /home/skyboxview/.bashrc
+fi
+
+echo "$DATE [INFO] - Script Complete"
+exit 0
